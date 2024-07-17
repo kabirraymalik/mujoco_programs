a1aa1fde83be05743a8579d0771883f7a3618ac5
Kabir Ray Malik <kabirraymalik@gmail.com>
2024-07-17 04:10:56 -0400

    added a few relevant models, and have live simulation frame by frame working now

--- live_sim.py
+++ live_sim.py
@@ -1,27 +1,37 @@
-with mujoco.viewer.launch_passive(m, d, key_callback=key_callback) as viewer:
+import time
 
-  # Enable wireframe rendering of the entire scene.
-  viewer.user_scn.flags[mujoco.mjtRndFlag.mjRND_WIREFRAME] = 1
-  viewer.sync()
+import mujoco
+import mujoco.viewer
 
+m = mujoco.MjModel.from_xml_path('resources/universal_robots_ur5e/scene.xml')
+d = mujoco.MjData(m)
+
+def key_listener(keycode):
+  if chr(keycode) == ' ':
+    global paused
+    paused = not paused
+
+paused = False
+
+with mujoco.viewer.launch_passive(m, d, key_callback=key_listener) as viewer:
+  # Close the viewer automatically after 30 wall-seconds.
+  start = time.time()
   while viewer.is_running():
-    ...
-    # Step the physics.
-    mujoco.mj_step(m, d)
+    step_start = time.time()
+    if not paused:
+        # mj_step can be replaced with code that also evaluates
+        # a policy and applies a control signal before stepping the physics.
+        mujoco.mj_step(m, d)
 
-    # Add a 3x3x3 grid of variously colored spheres to the middle of the scene.
-    viewer.user_scn.ngeom = 0
-    i = 0
-    for x, y, z in itertools.product(*((range(-1, 2),) * 3)):
-      mujoco.mjv_initGeom(
-          viewer.user_scn.geoms[i],
-          type=mujoco.mjtGeom.mjGEOM_SPHERE,
-          size=[0.02, 0, 0],
-          pos=0.1*np.array([x, y, z]),
-          mat=np.eye(3).flatten(),
-          rgba=0.5*np.array([x + 1, y + 1, z + 1, 2])
-      )
-      i += 1
-    viewer.user_scn.ngeom = i
-    viewer.sync()
-    ...
+        # Example modification of a viewer option: toggle contact points every two seconds.
+        with viewer.lock():
+            viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+
+        # Pick up changes to the physics state, apply perturbations, update options from GUI.
+        viewer.sync()
+
+        # Rudimentary time keeping, will drift relative to wall clock.
+        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
+
