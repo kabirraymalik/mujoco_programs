e68cf00eeeca34a577195eb694359858a957130d
kabirraymalik <66756718+kabirraymalik@users.noreply.github.com>
2024-09-04 06:12:35 -0700

    cleanup

--- tester.py
+++
@@ -1,9 +0,0 @@
-import mujoco
-import mujoco.viewer as viewer
-import os
-
-model_path = "mujoco_menagerie/universal_robots_ur5e/scene.xml"
-model = mujoco.MjModel.from_xml_path(model_path)
-data = mujoco.MjData(model)
-#mujoco.mj_step(model, data)
-viewer.launch(model, data)
